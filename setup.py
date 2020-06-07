000b9dc098516bfce804100173e4ceff20796bd1
taylor <tayjaybabee@gmail.com>
2020-06-07 01:10:19 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+        name='sub-dom-scan',
+        version='1.0',
+        packages=[ 'sub_dom_scan' ],
+        url='https://github.com/tayjaybabee/sub-dom-scan',
+        license='MIT',
+        author='Taylor-Jayde Blackstone',
+        author_email='t.blackstone@inspyre.tech',
+        description='Scans output files from SubDomSpy scans to see what hosts respond to pings.'
+)
